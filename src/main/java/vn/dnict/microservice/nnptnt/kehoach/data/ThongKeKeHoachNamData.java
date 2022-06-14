09a11be79ea0489e0f00bf5e99959639edd271f9
TheHung <53209749+thehung1999@users.noreply.github.com>
2022-06-14 08:00:07 +0700

    Hung commit kehoachnam

---
+++ src/main/java/vn/dnict/microservice/nnptnt/kehoach/data/ThongKeKeHoachNamData.java
@@ -0,0 +1,50 @@
+package vn.dnict.microservice.nnptnt.kehoach.data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class ThongKeKeHoachNamData {
+	
+	private Long keHoachNamId;	
+	private String keHoachNamTen;
+	private String khSoKyHieu;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private LocalDate khNgayBanHanh;
+	private Long khDonViChuTriId;
+	private String khDonViChuTriTen;
+	private Integer khNam;
+	
+	private String tenNhiemVu;
+
+	private Long nhiemVuChaId;
+
+	private Integer sapXep;
+
+	private String donViPhoiHop;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private LocalDate tuNgay;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private LocalDate denNgay;
+
+	private Long loaiNhiemVuId;
+	private String loaiNhiemVuTen;
+	private String loaiNvMa;
+
+	private String ghiChu;
+	
+	private Boolean tinhTrang;
+	
+	private Integer mucDoHoanThanh;
+	
+	private String ketQua;
+	
+	List<ThongKeKeHoachNamData> thongKeKeHoachDatas = new ArrayList<ThongKeKeHoachNamData>();
+}
